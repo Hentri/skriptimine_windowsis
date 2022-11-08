344d97765d9bf16d5ef95768ba984457400d76aa
Hentri <henri.sarapuu@voco.ee>
2022-11-08 15:38:39 +0200

    local account.ps1

---
+++ local account.ps1
@@ -0,0 +1,4 @@
+﻿# loome kasutaja jaoks parooli
+$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -force
+# loome kasutaja
+New-LocalUser "kasutaja1" -Password $KasutajaParool -FullName "esimene kasutaja" -Description "local account"
