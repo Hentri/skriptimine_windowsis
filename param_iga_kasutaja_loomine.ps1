cf827a487f7b0b754da6a89ffcaa7a9e5d8dc6aa
Hentri <henri.sarapuu@voco.ee>
2022-11-09 11:20:50 +0200

    param_iga_kasutaja_loomine.ps1

---
+++ param_iga_kasutaja_loomine.ps1
@@ -0,0 +1,10 @@
+﻿# vajalikud parameetrid
+param(
+$Kasutajanimi,
+$Taisnimi,
+$kontokirjeldus
+)
+# loome kasutajale parooli
+$kasutajaparool = ConvertTo-SecureString "Qwerty123" -AsPlainText -Force
+# lisame kasutaja vastavate andemetega
+New-Localuser $Kasutajanimi -Password $kasutajaparool -FullName $Taisnimi -Description $kontokirjeldus
