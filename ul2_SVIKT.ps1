4df9c79c6f23c08f804319c181628b75d4c113af
Hentri <henri.sarapuu@voco.ee>
2022-11-16 16:55:23 +0200

    ul2_SVIKT.ps1

---
+++ ul2_SVIKT.ps1
@@ -0,0 +1,19 @@
+﻿$Eesnimi = Read-host "Sisesta oma eesnimi"
+#read hostiga $perenimi
+$Perenimi = Read-Host "Sisesta oma perenimi"
+#$kasutajnimi sätestamine
+$KasutajaNimi = $Eesnimi.ToLower() + " " + $Perenimi.ToLower()
+#$täisnimi
+$TaisNimi =$Eesnimi + " " + $Perenimi
+#kasutajanime väljakirjutamine write-outputiga
+Write-Output "Kustutav kasutaja on $($KasutajaNimi)"
+Write-Output "Kasutaja $($KasutajaNimi) on kustutatud"
+#lülitame süsteemi teaded välja
+$ErrorActionPreference = 'SilentlyContinue'
+#kasutaja loomise command
+Remove-LocalUser $Kasutajanimi
+if(!$?){
+Write-Output "Tekkinud probleem kasutaja loomisega"
+}
+#süsteemi teaded tagasi sisse
+$ErrorActionPreference = 'stop'
